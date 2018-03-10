79e38b4528b1cb9a950a054fd45730713f4d778e
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2018-03-10 20:56:26 +0300

    Update ht-elements-toolbar-signin-menu.js

--- ht-elements-toolbar-signin-menu.js
+++ ht-elements-toolbar-signin-menu.js
@@ -1,10 +1,10 @@
 "use strict";
-import { LitElement, html } from "../@polymer/lit-element/lit-element.js";
-import { repeat } from "../lit-html/lib/repeat.js";
-import "../@polymer/paper-item/paper-item.js";
-import "../ht-toolbar-cart/ht-toolbar-cart.js";
-import "../ht-toolbar-balance/ht-toolbar-balance.js";
-import "../@polymer/paper-styles/default-theme.js";
+import { LitElement, html } from "@polymer/lit-element";
+import { repeat } from "lit-html/lib/repeat.js";
+import "@polymer/paper-item/paper-item.js";
+import "ht-toolbar-cart/ht-toolbar-cart.js";
+import "ht-toolbar-balance/ht-toolbar-balance.js";
+import "@polymer/paper-styles/default-theme.js";
 
 class HTElementsToolbarSigninMenu extends LitElement {
   render({
